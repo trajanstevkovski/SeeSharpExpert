433eca732fa3afd706263b574eedbb72e5aac76f
gordanajr <gordanajr@outlook.com>
2018-09-01 12:25:57 +0200

    Added Generic Repo

---
+++ SeeSharpExpert/SSE.Lottery.Data/IRepository.cs
@@ -0,0 +1,14 @@
+﻿using SSE.Lotery.Data.Model;
+using System.Linq;
+
+namespace SSE.Lottery.Data
+{
+    public interface IRepository<T> where T : IEntity
+    {
+        void Insert(T entity);
+        void Delete(T entity);
+
+        IQueryable<T> GetAll();
+        T GetById(int id);
+    }
+}
---
+++ SeeSharpExpert/SSE.Lottery.Data/Repository.cs
@@ -0,0 +1,40 @@
+﻿using SSE.Lotery.Data.Model;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SSE.Lottery.Data
+{
+    public class Repository<T> : IRepository<T> where T : class, IEntity
+    {
+        protected DbSet<T> DbSet;
+
+        public Repository(DbContext dbContext)
+        {
+            DbSet = dbContext.Set<T>();
+        }
+
+        public IQueryable<T> GetAll()
+        {
+            return DbSet;
+        }
+
+        public T GetById(int id)
+        {
+            return DbSet.Find(id);
+        }
+
+        public void Delete(T entity)
+        {
+            DbSet.Remove(entity);
+        }
+
+        public void Insert(T entity)
+        {
+            DbSet.Add(entity);
+        }
+    }
+}
